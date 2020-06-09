ec244d971951f8c088cfced749d5be9efcc60b2c
Kennedy <jkennedyr3@gmail.com>
2020-06-09 01:42:44 -0300

    color navbar

--- src/components/Menu/MenuDesktop.js
+++ src/components/Menu/MenuDesktop.js
@@ -7,15 +7,15 @@ export default function MenuDesktop() {
     <header>
       <img alt="Logo do icasa" className="logo" src={logo} />
       <div className="head">
-        <ul>
+        <ul class="ul">
           <div class="margin">
             <li><a class="" href="index.html">Início</a></li>
-            <li><a href="/">O clube</a></li>
-            <li><a href="/">Tabelas</a></li>
-            <li><a href="/">Notícias</a></li>
-            <li><a href="/">Programação</a></li>
-            <li><a href="/">Mídia</a></li>
-            <li><a href="/">Contato</a></li>
+            <li><a href="#">O clube</a></li>
+            <li><a href="#">Tabelas</a></li>
+            <li><a href="#">Notícias</a></li>
+            <li><a href="#">Programação</a></li>
+            <li><a href="#">Mídia</a></li>
+            <li><a href="#">Contato</a></li>
           </div>
           </ul>
       </div>
