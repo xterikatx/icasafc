fd0eec0993332038783d28f2ee272d8d3741c78c
Kennedy <jkennedyr3@gmail.com>
2020-06-19 03:45:12 -0300

    home 3

--- src/components/HomePage/mob.js
+++ src/components/HomePage/mob.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import AwsSliderStyles from 'react-awesome-slider/src/styles';
-import './styles.css';
+import './styles2.css';
 import './patrocinio.css';
 
 const newPhoto = require('../../assets/foto5.jpg');
@@ -10,52 +10,35 @@ const imgs = require('../../assets/banner-slider.jpg');
 
 function HomePage() {
   return (
-    <div className="all_components">
-      <AwesomeSlider className='aws-btn' >
+    <div className="all_componentss">
+      <AwesomeSlider className='aws-btn b' >
         <div data-src={imgs} />
         <div data-src={imgs} />
         <div data-src={imgs} />
       </AwesomeSlider>
       {/*section de noticias aqui*/}
 
-      <div className="newsJogoss">
-      <h3>Notícias de Jogos 2</h3>
-      <div className="newstwo">
-      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
-      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
-      <h6 className="date"><a>20/20/2020</a></h6>
-      </div>
-
-      <div className="newstwo">
-      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
-      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
-      <h6 className="date"><a>20/20/2020</a></h6>
-      </div>
-
-
-      <div className="newstwo">
-
-      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
-      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
-      <h6 className="date"><a>20/20/2020</a></h6>
-      </div>
-      </div>
-
-      <div className="newsJogos2">
+      <div className="newsJogosss">
       <h3>Notícias de Jogos</h3>
-        <div className="newstwo">
-          <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
-          <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
-          <h6 className="date"><a>20/20/2020</a></h6>
-        </div>
-
-      
+      <div className="newstwo">
+      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
+      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
+      <h6 className="date"><a>20/20/2020</a></h6>
       </div>
 
-      <div className="sponsorship">
-        <h3>Patrocinio</h3>
-        <iframe width="310" height="175" src="https://www.youtube.com/embed/wHV1TKkceow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe width="310" height="175" src="https://www.youtube.com/embed/wHV1TKkceow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div className="newstwo">
+      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
+      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
+      <h6 className="date"><a>20/20/2020</a></h6>
+      </div>
+
+
+      <div className="newstwo">
+
+      <a href="/Noticia"><img className="photonews" src={newPhoto} alt="foto" /></a>
+      <h4><a href="/Noticia">Veja as ultimas noticias relacionadas da internet</a></h4>
+      <h6 className="date"><a>20/20/2020</a></h6>
+      </div>
       </div>
 
       <section className="cta" >
