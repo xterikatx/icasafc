ff3cbeb82db6fbc55e2f9fce3a75d63b5c36fc97
Kennedy <jkennedyr3@gmail.com>
2020-06-19 00:49:10 -0300

    home 2

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import AwsSliderStyles from 'react-awesome-slider/src/styles';
 import './styles.css';
-import Patrocinio from './Patrocinio';
-import Address from './Address';
-
+import './patrocinio.css';
 const newPhoto = require('../../assets/foto5.jpg');
 const imagens = require('../../assets/banner.PNG');
 const imgs = require('../../assets/banner-slider.jpg');
@@ -65,8 +63,21 @@ function HomePage() {
         </div>
       </div>
 
-      <Patrocinio />
-      <Address />
+      <div className="sponsorship">
+        <h3>Patrocinio</h3>
+        <iframe width="310" height="175" src="https://www.youtube.com/embed/wHV1TKkceow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="310" height="175" src="https://www.youtube.com/embed/wHV1TKkceow" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
+      <section className="cta" >
+        <div className="inner">
+          <img className="img-address" alt=""/>
+          <h1>Nossa Localização</h1>
+          <p>Rua Frei Damião, 1720 - CEP: 63040-640 - Lagoa Seca - Juazeiro do Norte-CE</p>
+          <p>Fone-Fax: (88)3571- 3060.</p>
+        </div>
+      </section>
+
     </div>
 
   )
