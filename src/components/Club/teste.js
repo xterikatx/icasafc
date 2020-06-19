b0b2ec187bae5c191b6915465ab3be52629d91fe
Kennedy <jkennedyr3@gmail.com>
2020-06-18 22:52:26 -0300

    date and name

--- src/components/Club/teste.js
+++ src/components/Club/teste.js
@@ -42,7 +42,7 @@ function Club() {
     <div>
       <img alt="Logo do icasa" className="bannr" src={logo} />
         <h6 className="por">Por: Luis amstrong</h6>
-        <h6>30 abril 2020</h6>
+        <h6 className="dt">30 abril 2020</h6>
         <h1 className="til">Titulo da Notícia</h1>
         <Typography className="typo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </Typography>
