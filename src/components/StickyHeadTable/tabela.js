a4c0538ab100687ce63bc5bbdd398ecceee5bdee
Kennedy <jkennedyr3@gmail.com>
2020-06-22 20:38:12 -0300

    breadcrumbs

--- src/components/StickyHeadTable/tabela.js
+++ src/components/StickyHeadTable/tabela.js
@@ -14,6 +14,12 @@ import FontSizeChanger from 'react-font-size-changer';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+import { emphasize, withStyles } from '@material-ui/core/styles';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Chip from '@material-ui/core/Chip';
+import HomeIcon from '@material-ui/icons/Home';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 const logo = require('../../assets/logo.png');
 
 const columns = [
@@ -90,6 +96,27 @@ const useStyles = makeStyles({
   },
 });
 
+const StyledBreadcrumb = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey[100],
+    height: theme.spacing(3),
+    color: theme.palette.grey[800],
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.grey[300],
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(theme.palette.grey[300], 0.12),
+    },
+  },
+}))(Chip);
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
 function StickyHeadTable() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
@@ -154,6 +181,23 @@ function StickyHeadTable() {
       </Tabs>
     </Paper>
       </header>
+
+      <Breadcrumbs style={{marginLeft: '90px', marginTop: '15px', marginBottom: 5,}} aria-label="breadcrumb">
+      <StyledBreadcrumb
+        component="a"
+        href="#"
+        label="Home"
+        icon={<HomeIcon fontSize="small" />}
+        onClick={handleClick}
+      />
+      <StyledBreadcrumb component="a" href="#" label="Catalog" onClick={handleClick} />
+      <StyledBreadcrumb
+        label="Accessories"
+        deleteIcon={<ExpandMoreIcon />}
+        onClick={handleClick}
+        onDelete={handleClick}
+      />
+    </Breadcrumbs>
       <Typography className='tii' href="/Club" gutterBottom variant="itle1">
       TABELA BRASILEIRÃO SERIE B
       </Typography>
