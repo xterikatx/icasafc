9f22d48947222b242ee60dc24603f55d1152b034
Kennedy <jkennedyr3@gmail.com>
2020-06-23 17:24:33 -0300

    Noticia ajustado

--- src/components/News/newsmob.js
+++ src/components/News/newsmob.js
@@ -9,7 +9,21 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import './tyle.css';
 
-const logo = require('../../assets/foto8.png')
+import Toolbar from '@material-ui/core/Toolbar';
+import { MenuContainer } from '../styles';
+import clsx from 'clsx';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+
+const logo = require('../../assets/foto8.png');
+const logo2 = require('../../assets/logo.png')
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -37,8 +51,40 @@ const useStyles = makeStyles((theme) => ({
 }));
 function Club() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const preventDefault = (event) => event.preventDefault();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return(
     <div>
+      <MenuContainer
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img style={{marginLeft: '36%'}} alt="Logo do icasa" className="logo" src={logo2} />
+
+
+        </Toolbar>
+      </MenuContainer>
+
       <img alt="Logo do icasa" className="bannr" src={logo} />
         <h6 className="por">Por: Luis amstrong</h6>
         <h6 className="dt">30 abril 2020</h6>
@@ -117,6 +163,20 @@ function Club() {
       </ListItem>
     </List>
 
+    <React.Fragment>
+    <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component="div" style={{marginTop:100, }} />
+      </Container>
+      <Grid  style={{marginLeft: '30px', marginBottom: 5,}}>
+      <FacebookIcon />
+      <InstagramIcon />
+      </Grid>
+        <Typography style={{ margintTop: 30, padding:'10px',}} variant="body1" color="initial">
+        Associação Desportiva Recreativa e Cultural Icasa
+        Icasafc.com - Todos os direitos reservados.
+        </Typography>
+    </React.Fragment>
 
 
     </div>
